d5733ccc2c1bce8669007577eece808f03c0bbea
Sanphet Somjit <xsplaystation5@gmail.com>
2025-02-23 14:37:33 +0700

    GET: Search Data

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,9 +1,22 @@
+import { type NextRequest } from "next/server"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export async function GET() {
-    const posts = await prisma.post.findMany()
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const search       = searchParams.get('search') || ''
+    const category     = searchParams.get('category')
+    const sort         = searchParams.get('sort') || 'desc'
+
+    const posts = await prisma.post.findMany({
+        where: {
+            title: {
+                contains: search,
+                mode: 'insensitive',
+            }
+        }
+    })
     return Response.json(posts)
 }
 
